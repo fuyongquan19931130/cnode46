215a1386b1e0eae0ca587cabb2287dc51292e553
fu yongquan <437991478@qq.com>
2018-06-25 16:19:15 +0800

    搭建项目框架，配置路由，处理函数模块

---
+++ controllers/topic.js
@@ -0,0 +1,20 @@
+// 话题路由
+
+exports.showTopic = (req, res) => {
+	res.send('showTopic')
+}
+exports.handleTopic = (req, res) => {
+	res.send('handleTopic')
+}
+exports.showTopicID = (req, res) => {
+	res.send('showTopicID')
+}
+exports.showEdit = (req, res) => {
+	res.send('showEdit')
+}
+exports.handleEdit = (req, res) => {
+	res.send('handleEdit')
+}
+exports.handleDelete = (req, res) => {
+	res.send('handleDelete')
+}
